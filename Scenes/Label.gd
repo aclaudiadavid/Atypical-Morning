da30de4d33b5081a8f1d0875cd8ec5b556ff3a6f
aclaudiadavid <aclaudia.david2000@gmail.com>
2021-01-12 15:29:09 +0000

    high scores window

---
+++ Scenes/Label.gd
@@ -0,0 +1,4 @@
+extends Label
+
+func _ready():
+	self.text = "1º"
--- Scripts/PlayButton.gd
+++ Scripts/PlayButton.gd
@@ -2,3 +2,7 @@ extends Button
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Scenes/cutscene.tscn")
+
+
+func _on_Button_pressed():
+	pass # Replace with function body.
